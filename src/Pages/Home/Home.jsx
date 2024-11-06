c5e72cec7d9c82d321a38f862a8e836455c4e398
Wadecx <tyronmorcel@gmail.com>
2024-11-06 21:49:16 +0100

    Updates

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,46 +1,88 @@
-import Navbar from "../../Components/Navbar/Navbar"
-import "./home.css"
+import Navbar from "../../Components/Navbar/Navbar";
+import "./home.css";
+import apps from "../../Components/hooks/apps.json";
+import { useState, useEffect } from "react";
+
+// Import des icons
+
+import { User } from "lucide-react";
+import { Star } from "lucide-react";
 
 const Home = () => {
   return (
     <div className="home">
-        <Navbar/>
+      <Navbar />
 
-        <div className="menu-wrapper">
+      <div className="menu-wrapper">
+        <div className="leftSide">
+          <div className="rating">
+            <div className="details">
+              <div className="users">
+                <h4>
+                  Users <User />
+                </h4>
+                <h3>1234</h3>
+              </div>
+
+              <div className="stars">
+                <h4>
+                  Stars <Star />
+                </h4>
+                <h3>4.8/5</h3>
+              </div>
+
+              <div className="status">
+                <h4>Status</h4>
+                <h3>ONLINE</h3>
+              </div>
+            </div>
+            <div className="table">
+              <ul>
+                <li>Mark : Incroyable</li>
+                <li>Joe : How it is possible ?</li>
+                <li>Eden : ça marche super bien !</li>
+                <li>Josh : Le logiciel et vraiment bien !</li>
+              </ul>
+            </div>
+          </div>
+
+          <div className="code">
             <div className="leftSide">
-                <div className="rating">
-                    <div className="details">
-                        <div className="users">
-                            <h4>Users</h4>
-                            <h3>1234</h3>
-                        </div>
-
-                        <div className="stars">
-                            <h4>Stars</h4>
-                            <h3>4.8/5</h3>
-                        </div>
-                    </div>
-                    <div className="table">
-                        <ul>
-                            <li>comment</li>
-                            <li>comment</li>
-                            <li>comment</li>
-                            <li>comment</li>
-                        </ul>
-                    </div>
-                </div>
-
-                <div className="code">
-                    <h3>Project Code</h3>
-                </div>
+              <h3>Project code</h3>
+              <img src="img/pie.png" alt="" />
             </div>
-
             <div className="rightSide">
-
+              <ul>
+                <li>
+                  PYTHON ---- <span className="red">75%</span>
+                </li>
+                <li>
+                  C# ---- <span className="blue">15%</span>
+                </li>
+                <li>
+                  LIBS ---- <span className="yellow">10%</span>
+                </li>
+              </ul>
             </div>
+          </div>
         </div>
-    </div>
-  )
-}
 
-export default Home
+        <div className="rightSide">
+          <h3>Project Review</h3>
+          {apps.map((element) => {
+            return (
+              <div className="apps" key={element.name}>
+                <img src={element.image} alt="" />
+                <h3>{element.name}</h3>
+                <h3>{element.version}</h3>
+                <h3>{element.Status}</h3>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
